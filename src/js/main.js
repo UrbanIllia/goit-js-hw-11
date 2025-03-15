cb4ab5121b662e98f7f04ed1f44103df2b009347
Urban <urban.mutagen@gmail.com>
2025-03-15 19:01:33 +0200

    dz11-3

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
-import axios from 'axios';
 import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-const API_KEY = '49342840-003e2c292237fbf09de0074d9';
-const BASE_URL = 'https://pixabay.com/api/';
+import { createMarkup } from './render-functions';
+import { getPictures } from './pixabay-api.js';
 
 const formSearch = document.querySelector('.form');
 const listImages = document.querySelector('.gallery');
@@ -66,48 +62,4 @@ function onSearch(event) {
       hideLoader();
       iziToast.error({ title: 'Error', message: error.message });
     });
-}
-
-async function getPictures(query) {
-  try {
-    const response = await axios.get(BASE_URL, {
-      params: {
-        key: API_KEY,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error(error.response?.data?.message || 'Failed to fetch images');
-  }
-}
-
-function createMarkup(images) {
-  return images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `
-      <li class="gallery-item">
-        <a class="gallery-link" href="${largeImageURL}">
-          <img class="gallery-image" src="${webformatURL}" alt="${tags}" width="360" />
-        </a>
-        <div class="thumb-block">
-          <div class="block"><h2 class="title">Likes</h2><p class="amount">${likes}</p></div>
-          <div class="block"><h2 class="title">Views</h2><p class="amount">${views}</p></div>
-          <div class="block"><h2 class="title">Comments</h2><p class="amount">${comments}</p></div>
-          <div class="block"><h2 class="title">Downloads</h2><p class="amount">${downloads}</p></div>
-        </div>
-      </li>`
-    )
-    .join('');
 }
